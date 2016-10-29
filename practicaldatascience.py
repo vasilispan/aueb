ca802adc104686e8ce2bec0c5027e9905ef8929d
chrysanthoslianos <chrysanthoslianos@gmail.com>
2016-10-29 21:24:44 +0300

    Update practicaldatascience.py

--- practicaldatascience.py
+++ practicaldatascience.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+#csvs are placed in folder, named appropriately during download
 january = 'nodiverts/january.csv'
 february = 'nodiverts/february.csv'
 march = 'nodiverts/march.csv'
@@ -15,20 +15,32 @@ october ='nodiverts/october.csv'
 november = 'nodiverts/november.csv'
 december = 'nodiverts/december.csv'
 
-jan=pd.read_csv(january, dtype='str')
-feb=pd.read_csv(february, dtype='str')
-mar=pd.read_csv(march, dtype='str')
-apr=pd.read_csv(april, dtype='str')
-may=pd.read_csv(may, dtype='str')
-jun=pd.read_csv(june, dtype='str')
-jul=pd.read_csv(july, dtype='str')
-sep=pd.read_csv(september, dtype='str')
-aug=pd.read_csv(august,dtype='str')
-oct=pd.read_csv(october, dtype='str')
-nov=pd.read_csv(november, dtype='str')
-dec=pd.read_csv(december, dtype='str')
+#import all csv files
+jan=pd.read_csv(january,usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+feb=pd.read_csv(february, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+mar=pd.read_csv(march, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+apr=pd.read_csv(april, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST','DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+may=pd.read_csv(may, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+jun=pd.read_csv(june, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+jul=pd.read_csv(july, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+sep=pd.read_csv(september, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+oct=pd.read_csv(october, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+nov=pd.read_csv(november, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
+dec=pd.read_csv(december, usecols=['MONTH','CARRIER','FLIGHTS','ORIGIN','DEST', 'DEP_DELAY_NEW'], dtype={'FLIGHTS':np.int32})
 
+#concatenate all dataframes to one for the year 2015
 frames=[jan,feb,mar,apr,may,jun,jul,sep,oct,nov,dec]
 flights = pd.concat(frames,axis=0, ignore_index=True)
+#get sum of flights per airport
+flights_per_airport= flights.groupby(by=['ORIGIN'])['FLIGHTS'].sum()
+#map previous to 2015 dataframe
+flights['FLIGHTS_SUM'] = flights['ORIGIN'].map(flights_per_airport)
+# drop rows where we have <0.01
+flights2 = flights[flights.FLIGHTS_SUM > np.percentile(flights_per_airport,1)]
+flights2 = flights2.reset_index()
+#drop unnecessary columns for question1
+flights2=flights2.drop({'index','MONTH','CARRIER','DEST','FLIGHTS','FLIGHTS_SUM'},1)
+#and BAM we have 4 airports gone
+flights.ORIGIN.describe()  # -> unique 322
+flights2.ORIGIN.describe() # -> unique 317
 
-flightstest=flights.drop('YEAR',axis=1)
