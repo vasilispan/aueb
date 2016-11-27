363aa6122f3fbc644b796d7834e016d1836eed98
chrysanthoslianos <chrysanthoslianos@gmail.com>
2016-11-27 21:54:37 +0200

    Update statistics2.R

--- statistics2.R
+++ statistics2.R
@@ -1,4 +1,8 @@
 require(gdata)
 df = read.xls("Assignment_2_Data.xlsx" ,sheet ="Lottery_Data",header=TRUE)
 
-balls<-c(1:30)
+result <- apply(X = df,MARGIN=1, FUN= chisq.test)
+
+str(chisq.test(df[1, ]))
+
+plot(unlist(lapply(result, "[", "p.value"), use.names = FALSE))
