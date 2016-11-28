d4796cbe121a4df316285256a49a2ad9be88f48c
chrysanthoslianos <chrysanthoslianos@gmail.com>
2016-11-28 23:01:00 +0200

    Update statistics2.R

--- statistics2.R
+++ statistics2.R
@@ -1,26 +1,30 @@
 require(gdata)
 df = read.xls("Assignment_2_Data.xlsx" ,sheet ="Lottery_Data",header=TRUE)
 
-result <- apply(X = df,MARGIN=1, FUN= chisq.test)
-
-str(chisq.test(df[1, ]))
-
-plot(unlist(lapply(result, "[", "p.value"), use.names = FALSE))
-abline(h=0.05,col="red")
-print("Large amount of P-Values are  < 0.05 using Pearson's Chi-Squared test, so we do reject the null hypothesis 
-that there is equiprobability of data")
-
-plot_columns = function(x){
-	y<-sort(x)
-	hist(y,probability=TRUE,col="blue",main = paste("Histogram of columns of lottery_data"))
-	lines(density(y),col="red")
-	cat ("Press [enter] to continue")
-	line <- readline()
+apply_statistics = function(df){
+	result <- apply(X = df,MARGIN=1, FUN= chisq.test)
+	str(chisq.test(df[1, ]))
+	plot(unlist(lapply(result, "[", "p.value"), use.names = FALSE))
+	abline(h=0.05,col="red")
+	print("Large amount of P-Values are  < 0.05 using Pearson's Chi-Squared test, so we do reject the null hypothesis 
+	that there is equiprobability of data")
+	plot_columns = function(x){
+		y<-sort(x)
+		hist(y,probability=TRUE,col="blue",main = paste("Histogram of columns of lottery_data"))
+		lines(density(y),col="red")
+		cat ("Press [enter] to continue")
+		line <- readline()
+	}
+	print("plot histograms of each column of the data")
+	apply(df,2,function(x) plot_columns(x))
+	print("Apply chisq.test per column. We again reject the null hypothesis")
+	apply(df,2,chisq.test)
 }
-
-print("plot histograms of each column of the data")
-apply(df,2,function(x) plot_columns(x))
-print("Apply chisq.test per column. We again reject the null hypothesis")
-apply(df,2,chisq.test)
-
+print("apply statistics on full dataframe")
+apply_statistics(df)
+print("Choose extreme numbers away from the mean of 1:30")
 	
+print("New dataframe with first 100 rows only")
+apply_statistics(df[1:100,])
+apply_statistics(df[1:1000,])	      
+		
